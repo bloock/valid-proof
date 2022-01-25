fb14694a87fe69b37303c33c0ee9bdfddd4fee14
Cristina Castro <cristinacastro87@gmail.com>
2022-01-25 16:21:37 +0100

    Integrate JSON format field

--- src/components/JSONValidation.js
+++ src/components/JSONValidation.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+
 
 const JSONValidation = () => {
-    return(
-        <div>
-            <Form>
-                <Form.Group className="mb-3">
-                    <Form.Control as="textarea" placeholder="Paste your JSON here" rows={10} />
-                </Form.Group>
-            </Form>
-        </div>
-    )
-}
+
+
+
+    const handleChange = (e) =>{
+        console.log(e.target.value)
+    }
+    const handleSubmission = (e) =>{
+        function isJsonString(str) {
+            try {
+                JSON.parse();
+            } catch (e) {
+                return false;
+            }
+            return true;
+        }
+        console.log(isJsonString({"first_name":"Tony","last_name":"Hawk","age":31}))
+    }
+
+  return (
+    <div>
+      <Form>
+        <Form.Group className="mb-3 d-flex flex-column align-items-center">
+          <Form.Control
+            as="textarea"
+            placeholder="Paste your JSON here"
+            rows={10}
+            onChange={handleChange}
+          />
+          <button
+            className="button mt-3"
+            style={{ width: "30%" }}
+            onClick={handleSubmission}
+          >
+            Validate JSON
+          </button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+};
 
 export default JSONValidation;
