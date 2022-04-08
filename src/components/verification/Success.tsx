5ccc9075b8d93ceb8cb69beed615692dd41ffc65
mireiaGomariz <mireiapgomariz@gmail.com>
2022-04-08 12:27:03 +0200

    implement styles

--- src/components/verification/Success.tsx
+++ src/components/verification/Success.tsx
@@ -1,10 +1,13 @@
 import { Proof, Record } from "@bloock/sdk";
 import moment from "moment";
+import { Accordion, AccordionTab } from "primereact/accordion";
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
 import { DataTable } from "primereact/datatable";
 import { Divider } from "primereact/divider";
+import { Tag } from "primereact/tag";
 import React, { useState } from "react";
+import TooltipComponent from "./Tooltip";
 
 type VerificationSuccessProps = {
   fileName: string | null;
@@ -19,6 +22,8 @@ const VerificationSuccess: React.FC<VerificationSuccessProps> = ({
 }) => {
   const [expandedRows, setExpandedRows] = useState<any>(null);
 
+  console.log(recordProof);
+
   const tableNetworksData = (recordProof as any)?.anchor.networks.map(
     (network: any) => {
       const dates = moment(network.created_at * 1000).format(
@@ -31,7 +36,30 @@ const VerificationSuccess: React.FC<VerificationSuccessProps> = ({
           network.name === "ethereum_rinkeby"
             ? "Ethereum Rinkeby"
             : network.name,
-        state: network.state,
+        state: (
+          <Tag
+            icon={
+              network.state === "Confirmed"
+                ? "pi pi-check"
+                : "pi pi-exclamation-triangle"
+            }
+            style={
+              network.state === "Confirmed"
+                ? {
+                    fontWeight: "100",
+                    backgroundColor: "#DEF5F3",
+                    color: "#9CB3B1",
+                  }
+                : {
+                    fontWeight: "100",
+                    backgroundColor: "#DEF5F3",
+                    color: "#B3AA9C",
+                  }
+            }
+            severity={network.state === "Confirmed" ? "success" : "warning"}
+            value={network.state}
+          ></Tag>
+        ),
         tx_hash: network.tx_hash,
       };
     }
@@ -97,41 +125,64 @@ const VerificationSuccess: React.FC<VerificationSuccessProps> = ({
           </p>
         </div>
       </div>
-      <div className="bold-text">Document hash</div>
+      <div className="bold-text">
+        <TooltipComponent title="Document hash" description="Test description">
+          <p>
+            Document hash{" "}
+            <i className="pi pi-question-circle px-1 py-1 text-secondary "></i>
+          </p>
+        </TooltipComponent>
+      </div>
       <div style={{ overflowWrap: "break-word" }}>
         {record && record.getHash()}
       </div>
-
       <Divider className="my-2 pb-2" />
-
-      {recordProof ? (
-        <>
-          <div className="bold-text">Anchor</div>
-          <div>{(recordProof as any).anchor.anchor_id}</div>
-
-          <Divider className="my-2 pb-2" />
-        </>
-      ) : null}
-
-      <div className="bold-text">Networks</div>
-      <div className="card my-3">
-        <DataTable
-          value={tableNetworksData}
-          expandedRows={expandedRows}
-          onRowToggle={(e) => setExpandedRows(e.data)}
-          rowExpansionTemplate={rowExpansionTemplate}
-          responsiveLayout="scroll"
-          dataKey="id"
-        >
-          <Column expander style={{ width: "3em" }} />
-          <Column field="label" header="Name"></Column>
-          <Column field="state" header="State"></Column>
-          <Column field="created_at" header="Timestamp"></Column>
-        </DataTable>
+      <div className="bold-text">
+        <TooltipComponent title="Issuer" description="Issuer description">
+          <p>
+            Issuer
+            <i className="pi pi-question-circle px-1 py-1 text-secondary "></i>
+          </p>
+        </TooltipComponent>
       </div>
-      <Divider className="my-2 pb-2" />
-      <div className="bold-text">Issuer</div>
       <div>BLOOCK</div>
+      <Divider className="my-2 pb-2" />
+      <Accordion>
+        <AccordionTab header="Detailed info" contentClassName="bg-light">
+          {recordProof ? (
+            <>
+              <Divider className="my-2 pb-2" />
+
+              <div className="bold-text">Anchor</div>
+              <div>{(recordProof as any).anchor.anchor_id}</div>
+
+              <Divider className="my-2 pb-2" />
+              <div className="bold-text">Root</div>
+              <div style={{ overflowWrap: "break-word" }}>
+                <div>{(recordProof as any).root}</div>
+              </div>
+              <Divider className="my-2 pb-2" />
+            </>
+          ) : null}
+          <div className="bold-text">Networks</div>
+          <div className="card my-3">
+            <DataTable
+              value={tableNetworksData}
+              expandedRows={expandedRows}
+              onRowToggle={(e) => setExpandedRows(e.data)}
+              rowExpansionTemplate={rowExpansionTemplate}
+              responsiveLayout="scroll"
+              dataKey="id"
+            >
+              <Column expander style={{ width: "3em" }} />
+              <Column field="label" header="Name"></Column>
+              <Column field="state" header="State"></Column>
+              <Column field="created_at" header="Timestamp"></Column>
+            </DataTable>
+          </div>
+          <Divider className="my-2 pb-2" />
+        </AccordionTab>
+      </Accordion>
     </>
   );
 };
---
+++ src/components/verification/Tooltip.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { OverlayTrigger, Popover } from "react-bootstrap";
+
+type TooltipProps = {
+  title: string;
+  description: string;
+  children: any;
+};
+
+const TooltipComponent: React.FC<TooltipProps> = ({
+  title,
+  description,
+  children,
+}) => {
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">{title}</Popover.Header>
+      <Popover.Body>{description}</Popover.Body>
+    </Popover>
+  );
+
+  return (
+    <OverlayTrigger overlay={popover} placement="top" trigger="hover">
+      {children}
+    </OverlayTrigger>
+  );
+};
+
+export default TooltipComponent;
