65c3f85fc0bd6983470ba6fe55585a62d93ee590
marcbaque <marc@bloock.com>
2024-01-03 18:03:52 +0100

    Add support for changing Bloock API Host

--- src/services/BloockService.ts
+++ src/services/BloockService.ts
@@ -7,6 +7,8 @@ import {
   LocalKey,
   ManagedCertificate,
   ManagedKey,
+  Bloock,
+  Network,
 } from "@bloock/sdk";
 import {
   convertAnchorNetworkToNetwork,
@@ -30,6 +32,23 @@ export default class BloockService {
   private bloockClient: BloockClient;
 
   constructor() {
+    const apiHost = import.meta.env.VITE_API_HOST;
+    if (apiHost && apiHost.includes("api.bloock.dev")) {
+      Bloock.setApiHost(apiHost);
+
+      Bloock.setProvider(Network.BLOOCK_CHAIN, "https://ganache.bloock.dev");
+      Bloock.setContractAddress(
+        Network.BLOOCK_CHAIN,
+        "d2d1BBcbee7741f8C846826F55b7c17fc5cf969a"
+      );
+
+      Bloock.setProvider(Network.ETHEREUM_GOERLI, "https://goerli.bloock.dev");
+      Bloock.setContractAddress(
+        Network.ETHEREUM_GOERLI,
+        "C8a9f5D79f94cbE4DA1171783F03976594b60c95"
+      );
+    }
+
     this.bloockClient = new BloockClient(import.meta.env.VITE_API_KEY);
   }
 
