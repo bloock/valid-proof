a9ab151f85b4d94f85a2ddb3e222372066eb4182
marcbaque <marc@bloock.com>
2024-05-13 11:49:13 +0200

    resolve compatibility issues with pdf-lib v4.2.67

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
-import { Plugin, ResolvedConfig, defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +12,12 @@ export default defineConfig({
   optimizeDeps: {
     include: ["@bloock/sdk > protobufjs/minimal"],
     exclude: ["@bloock/sdk"],
+    esbuildOptions: {
+      target: "esnext",
+    },
+  },
+  build: {
+    target: "esnext",
   },
 });
 
