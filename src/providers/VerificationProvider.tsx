decad68d2f7418b30706e162db92587831cce58c
Mireia Pérez Gomàriz <47714488+mireiaGomariz@users.noreply.github.com>
2024-07-23 18:19:14 +0200

    BIT-97 - Add directory support and visualization (#81)
    * add directory support and visualization
    
    * reorganize functions and unify IPFSCid type
    
    * file extension
    
    * IPFSCid
    
    * IPFS Cid model
    
    * delete file png

--- src/providers/VerificationProvider.tsx
+++ src/providers/VerificationProvider.tsx
@@ -1,9 +1,11 @@
 import {
   AccessControl,
+  IpfsLoader,
   LocalCertificate,
   LocalKey,
   ManagedCertificate,
   ManagedKey,
+  RecordClient,
 } from "@bloock/sdk";
 import React, {
   createContext,
@@ -24,9 +26,13 @@ import {
   IntegrityDetails,
 } from "../models/VerificationResult";
 import BloockService from "../services/BloockService";
+import DirectoryPreview from "../components/DirectoryPreview";
+import { DirectoryResponse } from "../models/ReadDirectory";
+import { IPFSCid } from "../models/ReadDirectory";
 
 export type VerificationState = {
-  onInputChange: (input: File | URL) => void;
+  onInputChange: (input: File | URL | IPFSCid) => void;
+  onDirectory: (directory: URL) => void;
   onDecryptFile: (
     key: LocalKey | LocalCertificate | ManagedKey | ManagedCertificate,
     accessControl?: AccessControl
@@ -37,6 +43,7 @@ export type VerificationState = {
   encryptionDetails: EncryptionDetails | undefined;
   availabilityDetails: AvailabilityDetails | undefined;
   service: BloockService;
+  directoryResponse: DirectoryResponse | undefined;
   reset: () => void;
 };
 
@@ -58,6 +65,9 @@ export const useVerification = () => {
 export const steps = {
   loader: {
     Contents: FileLoader,
+  },
+  directory: {
+    Contents: DirectoryPreview,
   },
   decrypt: {
     Contents: DecryptFile,
@@ -88,6 +98,9 @@ export const VerificationProvider: React.FC = () => {
   >();
 
   const [component, setComponent] = useState(steps.loader);
+
+  const [directoryResponse, setDirectoryResponse] =
+    useState<DirectoryResponse>();
 
   const bloockService = useMemo(() => {
     return new BloockService();
@@ -177,7 +190,7 @@ export const VerificationProvider: React.FC = () => {
       });
   };
 
-  const onInputChange = (input: File | URL | Uint8Array) => {
+  const onInputChange = (input: File | URL | Uint8Array | IPFSCid) => {
     let readPromise: Promise<AvailabilityDetails>;
     if (input instanceof URL) {
       readPromise = bloockService.readUrl(input);
@@ -185,6 +198,8 @@ export const VerificationProvider: React.FC = () => {
       readPromise = bloockService.readFile(input);
     } else if (input instanceof Uint8Array) {
       readPromise = bloockService.readBuffer(input, availabilityDetails);
+    } else if (input instanceof IPFSCid) {
+      readPromise = bloockService.readCID(input);
     } else {
       return;
     }
@@ -198,25 +213,42 @@ export const VerificationProvider: React.FC = () => {
       .catch(console.error);
   };
 
-  const reset = () => {
-    if (searchParams.has("url")) {
-      searchParams.delete("url");
-      setSearchParams(searchParams);
+  const onDirectory = async (directory: URL) => {
+    let readDirectory: DirectoryResponse;
+    setComponent(steps.directory);
+
+    if (directory) {
+      readDirectory = await fetch(directory.href).then((response) => {
+        return response.json();
+      });
+      setDirectoryResponse(readDirectory);
     }
+  };
+
+  const reset = () => {
+    ["url", "dir"].forEach((param) => {
+      if (searchParams.has(param)) {
+        searchParams.delete(param);
+        setSearchParams(searchParams);
+      }
+    });
 
     setIsFileValid(undefined);
     setIntegrityDetails(undefined);
     setAuthenticityDetails(undefined);
     setEncryptionDetails(undefined);
     setAvailabilityDetails(undefined);
+    setDirectoryResponse(undefined);
     setComponent(steps.loader);
 
     window.scrollTo(0, 0);
   };
 
   const value: VerificationState = {
+    onDirectory,
     onInputChange,
     onDecryptFile,
+    directoryResponse,
     isFileValid,
     integrityDetails,
     authenticityDetails,
