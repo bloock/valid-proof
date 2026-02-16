dc542f3c1af73dda0539a779076a6e584253ba94
Carlos <carlosmartgom@gmail.com>
2026-02-16 20:10:42 +0100

    correct compilation errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,14 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly NODE_ENV: 'development' | 'production'
   readonly VITE_API_KEY: string
   readonly VITE_SENTRY_DSN: string
   readonly VITE_API_HOST?: string
+  readonly VITE_PRIMARY_COLOR?: string
+  readonly VITE_FONT_FAMILY?: string
+  readonly VITE_BACKGROUND_IMAGE?: string
+  readonly VITE_LOGO?: string
 }
 
 interface ImportMeta {
