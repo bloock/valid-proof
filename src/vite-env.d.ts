81a2f2e90b1d6ce57d4f778256f975f9f5736ce1
Carlos <carlosmartgom@gmail.com>
2026-02-16 19:55:46 +0100

    correct compilation errors

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_KEY: string
+  readonly VITE_SENTRY_DSN: string
+  readonly VITE_API_HOST?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
